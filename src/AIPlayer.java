b7b1588f759616f760d2d98b56ceaf98e423f691
Kibugi Kamau <kmbugua@drew.edu>
2017-06-15 16:12:32 -0400

    added basic Player and AIPlayer classes

---
+++ src/AIPlayer.java
@@ -0,0 +1,19 @@
+
+public class AIPlayer extends Player{
+	
+	private String difficulty;
+	
+	public Move makeMove() {
+		return new Move(1,1);
+	}
+	
+	public Move predictPlayer() {
+		return new Move(1,1);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
