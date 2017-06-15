4a377e768552f42fab87c559d06e6837f7181e83
Sindy Alzate <sindyalzate@Sindys-MacBook.local>
2017-06-15 16:41:56 -0400

    MouseListner & paintcomponent

--- src/BoardView.java
+++ src/BoardView.java
@@ -1,28 +1,41 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class BoardView extends JPanel {
 	
 private Circle board;
-private Rectangle circle;
-
+private Rectangle boardBase;
 
 public BoardView(String string) {
-	// TODO Auto-generated constructor stub
+	this.addMouseListener(new MouseAdapter(){
+		public void mouseClicked(MouseEvent e){
+			board.setColor(Color.YELLOW);
+board = new Circle(200,170,20, Color.yellow);
+		}
+	});
 }
 
 public void reset(){
 	
 }
 
-public void displyEndGame(){
+public void displayEndGame(){
 	
 }
 
 public void Move(){
 	
 }
-		
+
+public void paintComponent(Graphics page){
+	board.paint(page);
+	
+}
 
 public static void main(String[] args) {
 	JFrame frame = new JFrame ();
