994d762ba42a934de5f2a0cad6ecdf3a13162e12
Emily Hill <emhill@drew.edu>
2017-06-15 15:24:38 -0400

    Initial commit

---
+++ src/Move.java
@@ -0,0 +1,24 @@
+
+public class Move {
+	private int x, y;
+	
+	public Move(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * @return the x
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * @return the y
+	 */
+	public int getY() {
+		return y;
+	}
+	
+}
