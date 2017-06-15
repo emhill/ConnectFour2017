4cdd9b268e7a3b8f8c40d428ae5b86c5f51daa68
Parth Patel <parthpatel62496@gmail.com>
2017-06-15 16:03:41 -0400

    Preliminary UML Implimentation

--- src/GameSetupView.java
+++ src/GameSetupView.java
@@ -1,9 +1,28 @@
-//Parth Patel
-public class GameSetupView {
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+//Parth Patel
+public class GameSetupView extends JPanel{
+	private String GameName;
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		JFrame frame = new JFrame("GameSetupView");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new GameSetupView(); 
+		panel.setPreferredSize(new Dimension(700,700));
+		
+		frame.getContentPane().add(panel);
+		frame.pack();
+		frame.setVisible(true);
+		
 
 	}
 
-}
+	}
+
