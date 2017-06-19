6a08fdc9073ed59009c4a5ad4291e3a0ac941499
HASSAN <hsaeed@drew.edu>
2017-06-19 10:49:40 -0400

    Board view

--- src/BoardView.java
+++ src/BoardView.java
@@ -1,39 +1,7 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-public class BoardView extends JPanel {
+public class BoardView {
 	
-private Circle board;
-private Rectangle circle;
 
-
-public BoardView(String string) {
-	// TODO Auto-generated constructor stub
-}
-
-public void reset(){
 	
-}
 
-public void displyEndGame(){
-	
-}
-
-public void Move(){
-	
-}
-		
-
-public static void main(String[] args) {
-	JFrame frame = new JFrame ();
-	BoardView phrase = new BoardView("Random String!");
-	//		phrase.revealLetter('a');
-	
-//	phrase.revealFullPhrase();
-	JPanel panel = phrase;
-
-	frame.getContentPane().add(panel);
-	frame.pack();
-	frame.setVisible(true);
-}
 }
