9b7d39b9e4c30763990b42945305d8bd4acf445a
Sindy Alzate <sindyalzate@Sindys-MacBook.local>
2017-06-19 13:04:11 -0400

    Board Rect

--- src/BoardView.java
+++ src/BoardView.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -12,13 +13,18 @@ private Circle board;
 private Rectangle boardBase;
 
 public BoardView(String string) {
+	boardBase = new Rectangle(50,90,500, 500, Color.yellow);
+	this.setPreferredSize(new Dimension(700,700));
+	board = new Circle(200,170,20, Color.orange);
 	this.addMouseListener(new MouseAdapter(){
-		public void mouseClicked(MouseEvent e){
-			board.setColor(Color.YELLOW);
-board = new Circle(200,170,20, Color.yellow);
+		public void mouseMoved(MouseEvent e){
+			board.setColor(Color.blue);
+			// use mouse event to get x,y and change circles x,y
+			repaint();
 		}
-	});
+	});	
 }
+	
 
 public void reset(){
 	
@@ -33,6 +39,7 @@ public void Move(){
 }
 
 public void paintComponent(Graphics page){
+	boardBase.paint(page);
 	board.paint(page);
 	
 }
