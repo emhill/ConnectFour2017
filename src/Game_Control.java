206f0966732a9f764748a7098b78a9e0f9eb5521
Parth Patel <parthpatel62496@gmail.com>
2017-06-19 11:29:36 -0400

    Completed Initial Player Inputs

--- src/Game_Control.java
+++ src/Game_Control.java
@@ -1,6 +1,10 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+ 
+public class Game_Control extends JFrame {
 
-public class Game_Control {
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
