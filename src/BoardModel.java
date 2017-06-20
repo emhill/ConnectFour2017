7a35ffc661f8135b671c4f6a84169b4b96410145
Kibugi Kamau <kmbugua@drew.edu>
2017-06-20 16:46:58 -0400

    changed function to determine row

--- src/BoardModel.java
+++ src/BoardModel.java
@@ -46,26 +46,26 @@ public class BoardModel {
 	}
 	
 	public boolean isOver(){
-//		int nullCounter = 0;
-//		for (int row = 0; row < board.length; row++ ) {
-//			for (int column = 0; column < board[row].length; column++) {	
-//				if (board[row][column] == null)
-//					nullCounter ++;
-//				if (four(row, column) == true)
-//					return true;
-//			}
-//		}
-//		if (nullCounter == 0)
-//			return true;
+		int nullCounter = 0;
+		for (int row = 0; row < board.length; row++ ) {
+			for (int column = 0; column < board[row].length; column++) {	
+				if (board[row][column] == null)
+					nullCounter ++;
+				if (four(row, column) == true)
+					return true;
+			}
+		}
+		if (nullCounter == 0)
+			return true;
 		return false;
 	}
 	
 	public int columnRow(int column) {
-		for (int row = 0; row < board.length; row++) {
-			if (board[row][column] != null)
-				return row;
+		int row = board.length-1;
+		while (row > 0 && board[row][column] != null) {
+			row --;
 		}
-		return board.length-1;
+		return row;
 	}
 	
 	public void print(){
