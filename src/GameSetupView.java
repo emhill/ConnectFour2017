9d076ff39cfd1b6b4fe8638ae2be77b9d463dca3
Parth Patel <parthpatel62496@gmail.com>
2017-06-20 17:16:09 -0400

    Final Comments

--- src/GameSetupView.java
+++ src/GameSetupView.java
@@ -1,3 +1,6 @@
+//Parth Patel
+
+//Imports
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Frame;
@@ -9,17 +12,20 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Color;
 
-
-//Parth Patel
+//GameSetupView Class
 public class GameSetupView extends JPanel{
+	
+//Fields
 	private static Frame frame;
 	private static Icon icon;
 	private String Name;
 	private String Difficulty;
 	private Color Color;
 	
+//GameSetupView Constructor
 public GameSetupView(){
 		
+//Dialog Boxes for User Game Setup
 	 Name = (String)JOptionPane.showInputDialog(
                 frame,
                 "Insert Name here:\n",
@@ -48,6 +54,8 @@ public GameSetupView(){
                 icon,
                 colors,
                 "");
+		
+//Conversions from User Choices to actual Colors
 		if (color == "Red"){
 			Color = Color.RED;
 		}
@@ -61,52 +69,8 @@ public GameSetupView(){
 			Color = Color.GREEN;
 		}
 }
-//	 Name = (String)JOptionPane.showInputDialog(
-//                frame,
-//                "Insert Name here:\n",
-//                "Connect Four",
-//                JOptionPane.PLAIN_MESSAGE,
-//                icon,
-//                null,
-//                "");
-//
-//		Object[] possibilities = {"Easy", "Intermediate", "Hard"};
-//		Difficulty = (String)JOptionPane.showInputDialog(
-//                frame,
-//                "Please Select Difficulty Level:",
-//                "Connect Four",
-//                JOptionPane.PLAIN_MESSAGE,
-//                icon,
-//                possibilities,
-//                "");
-//		
-//		Object[] colors = {"Red", "Yellow", "Blue", "Green"};
-//		color = (String)JOptionPane.showInputDialog(
-//                frame,
-//                "Please Select A Color:",
-//                "Connect Four",
-//                JOptionPane.PLAIN_MESSAGE,
-//                icon,
-//                colors,
-//                "");
-		
-		
-//		Text name = new Text(Name);
-//		name.setPreferredSize(new Dimension(650, 50));
-//		this.add(name);
-//		
-//		
-//		Text difficulty = new Text(Difficulty);
-//		difficulty.setPreferredSize(new Dimension(650, 50));
-//		this.add(difficulty);
-//		
-//		
-//		Text color = new Text(Color);
-//		color.setPreferredSize(new Dimension(650, 50));
-//		this.add(color);
-//		
 
-
+//Get Methods for Player Inputs
 public String getName() {
 	return Name;
 }
@@ -119,9 +83,10 @@ public Color getColor() {
 	return Color;
 }
 
-
+//Main 
 public static void main(String[] args){
-	//GameSetupView ConnectFour = new GameSetupView();
+	//Creation of Connect Four Game Window
+	
 	JFrame frame = new JFrame("Connect Four");
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	
